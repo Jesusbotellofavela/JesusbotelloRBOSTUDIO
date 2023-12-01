8c90d9abf4ed16523bcf83b1199cd56131c1176d
Jesusbotellofavela <jesusbotellofavela@gmail.com>
2023-11-30 20:53:03 -0600

    agregamos el estilo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 <body>
     <div id="app">
